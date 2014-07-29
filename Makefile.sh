a9d4efd302a45f8f631bdd169b63a67ed4cbdd09
Dan Stephenson <ispyhumanfly@gmail.com>
2014-07-29 01:55:02 +0000

    So many changes. But now the installation and structure of the software is more clear. More to follow.

---
+++ Makefile.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+export EUPHORIA_INSTALL=`pwd`
+export SBBSCTRL=$EUPHORIA_INSTALL/sbbs/ctrl
+
+# Build Synchronet
+mkdir sbbs; cd sbbs/
+
+wget ftp://vert.synchro.net/Synchronet/sbbs_src.tgz
+wget ftp://vert.synchro.net/Synchronet/sbbs_run.tgz
+tar -xzf sbbs_src.tgz
+tar -xzf sbbs_run.tgz
+cd $EUPHORIA_INSTALL/sbbs/src/sbbs3; make RELEASE=1
+cd $EUPHORIA_INSTALL/sbbs/src/sbbs3/scfg; make RELEASE=1
+cd $EUPHORIA_INSTALL/sbbs//sbj; make
+cd $EUPHORIA_INSTALL/sbbs/sbl; make
+cd $EUPHORIA_INSTALL/sbbs/exec/
+
+ln -s $EUPHORIA_INSTALL/sbbs/src/sbbs3/gcc.*.exe.release/* .
+ln -s $EUPHORIA_INSTALL/sbbs/src/sbbs3/scfg/gcc.*.exe.release/scfg* .
+make
+
+# Integrate Phoenix_s3
+cd $EUPHORIA_INSTALL/sbbs/exec/
+
+mv login.js login.js.original
+ln -s $EUPHORIA_INSTALL/phoenix_s3/telnet/login.js login.js
+
+mv logon.js logon.js.original
+ln -s $EUPHORIA_INSTALL/phoenix_s3/telnet/logon.js logon.js
+ln -s $EUPHORIA_INSTALL/phoenix_s3/telnet/phoenix.js phoenix.js
+
+# Create Service Scripts
+#touch euphoria-start.js
+#touch euphoria-stop.js
+#touch euphoria-restart.js
+#touch euphroia-config.js
+
+cd $EUPHORIA_INSTALL ; touch euphoria.sh ; chmod +x euphoria.sh
+echo "#!/bin/sh" >> euphoria.sh
+echo "export SBBSCTRL=$EUPHORIA_INSTALL/sbbs/ctrl" >> euphoria.sh
+echo "$EUPHORIA_INSTALL/sbbs/exec/sbbs" >> euphoria.sh
+
+
+ 
+
+
--- README.md
+++ README.md
@@ -1,5 +1,18 @@
 # Euphoria
 
-More information wll be placed here.
+The future of 
+# Installation. 
+
+1. Download the master branch and run Makefile.sh wherever you want euphoria installed.
+2. The make process will download and setup Synchronet automatically. 
+3. Run: sudo ./euphoria.sh to interact with your new BBS.
+
+# Please report bugs.
+http://github.com/thoughtpivot.com/issues
+
+# Copyright
+None just yet, but we'll figure that out soon.
+
+
 
 
