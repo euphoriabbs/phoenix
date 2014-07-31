bac9b61dae07ac7fa9180077f408ded769f811c9
Dan Stephenson <ispyhumanfly@gmail.com>
2014-07-31 21:05:13 +0000

    The Makefile syste is working prety well. the phoenix.sbbs_config.ini file still has some issues, but i'll get to that.

--- Makefile.sh
+++ Makefile.sh
@@ -22,24 +22,20 @@ ln -s $EUPHORIA_INSTALL/sbbs/src/sbbs3/gcc.*.exe.release/* .
 ln -s $EUPHORIA_INSTALL/sbbs/src/sbbs3/scfg/gcc.*.exe.release/scfg* .
 make
 
-# Integrate Phoenix_s3
-cd $EUPHORIA_INSTALL
-mv euphoria-master/* .
-rm -rf euphoria-master/ && master.tar.gz
-
-cd $EUPHORIA_INSTALL/sbbs/exec/mods/
-ln -s $EUPHORIA_INSTALL/phoenix_s3/telnet/login.js login.js
-ln -s $EUPHORIA_INSTALL/phoenix_s3/telnet/logon.js logon.js
-ln -s $EUPHORIA_INSTALL/phoenix_s3/telnet/phoenix.js phoenix.js
-
-# Create Euphoria Daemon
+cd $EUPHORIA_INSTALL/sbbs/exec/
+mv login.js login.js-original
+mv logon.js logon.js-original
+ln -s $EUPHORIA_INSTALL/phoenix_s3/telnet/login.js $EUPHORIA_INSTALL/sbbs/exec/login.js
+ln -s $EUPHORIA_INSTALL/phoenix_s3/telnet/logon.js $EUPHORIA_INSTALL/sbbs/exec/logon.js
+ln -s $EUPHORIA_INSTALL/phoenix_s3/telnet/phoenix.js $EUPHORIA_INSTALL/sbbs/exec/phoenix.js
 
 cd $EUPHORIA_INSTALL ; touch euphoria.sh ; chmod +x euphoria.sh
 echo "#!/bin/sh" >> euphoria.sh
 echo "export SBBSCTRL=$EUPHORIA_INSTALL/sbbs/ctrl" >> euphoria.sh
 echo "$EUPHORIA_INSTALL/sbbs/exec/sbbs" >> euphoria.sh
 
-# Synchronet Configuration
+cd $EUPHORIA_INSTALL/sbbs ; ln -s ../phoenix_s3/ .
+
 mv $EUPHORIA_INSTALL/sbbs/ctrl/sbbs.ini $EUPHORIA_INSTALL/sbbs/ctrl/sbbs.ini-original
 ln -s $EUPHORIA_INSTALL/phoenix_s3/config/phoenix.sbbs_config.ini $EUPHORIA_INSTALL/sbbs/ctrl/sbbs.ini 
 
