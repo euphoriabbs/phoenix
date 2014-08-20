4a7eb40df329f30a618fdb1200bf913007af90a2
Dan Stephenson <ispyhumanfly@gmail.com>
2014-08-20 05:15:20 +0000

    More local fixes.

--- euphoria.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-export SBBSCTRL=/home/ubuntu/Euphoria/euphoria-master/sbbs/ctrl
-/home/ubuntu/Euphoria/euphoria-master/sbbs/exec/sbbs
-#!/bin/sh
-export SBBSCTRL=/home/ubuntu/Euphoria/euphoria-master/sbbs/ctrl
-/home/ubuntu/Euphoria/euphoria-master/sbbs/exec/sbbs
